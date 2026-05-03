42052109b8a44a82cf8d78e9fbd15e49a1273d36
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2026-05-03 14:39:03 +0200

    decompile tres

--- src/031_Land/MapObject/MapObjectUnkTRES.cpp
+++ src/031_Land/MapObject/MapObjectUnkTRES.cpp
@@ -1,22 +1,43 @@
-//! TODO: This file was generated automatically and might contain errors
-
 #include "MapObject/MapObjectUnkTRES.hpp"
 #include "System/SysNew.hpp"
+#include "Unknown/Common.hpp"
+#include "Unknown/UnkStruct_027e0cd8.hpp"
+
+static const Vec2p data_ov031_021157b0(0x96B, 0x99A);
 
 ARM DECL_PROFILE(MapObjectProfileUnkTRES);
 
 ARM MapObject *MapObjectProfileUnkTRES::Create() {
-    return new(HeapIndex_2) MapObjectUnkTRES();
+    return new(HeapIndex_ITCM) MapObjectUnkTRES();
 }
 
 ARM MapObjectProfileUnkTRES::MapObjectProfileUnkTRES() :
-    MapObjectProfile(MapObjectId_TRES) {}
+    MapObjectProfile_Derived2(MapObjectId_TRES, MapObjectId_TRES) {
+    this->mUnk_0E = 1;
+    this->mUnk_06 = 1;
+    this->mUnk_0C = 0x1333;
+}
 
-ARM MapObjectUnkTRES::MapObjectUnkTRES() {}
+ARM MapObjectUnkTRES::MapObjectUnkTRES() :
+    MapObjectChestBase(1) {
+    this->mUnk_074.vfunc_08(GetUnkPointer1<MapObjectProfileUnkTRES>());
+}
 
-ARM void MapObjectUnkTRES::func_ov031_02102f64(void) {}
-ARM void MapObjectUnkTRES::func_ov031_02102f90(void) {}
-ARM void MapObjectUnkTRES::func_ov031_02102f98(void) {}
+ARM unk32 MapObjectUnkTRES::vfunc_3C() {
+    if (data_027e0cd8->func_ov000_02081f3c(this->mUnk_20.mUnk_14, 1)) {
+        return 8;
+    }
+
+    return 3;
+}
+
+ARM bool MapObjectUnkTRES::vfunc_40() {
+    return true;
+}
+
+ARM Vec2p *MapObjectUnkTRES::vfunc_54() {
+    return (Vec2p *) &data_ov031_021157b0;
+}
 
 ARM MapObjectUnkTRES::~MapObjectUnkTRES() {}
 ARM MapObjectProfileUnkTRES::~MapObjectProfileUnkTRES() {}
