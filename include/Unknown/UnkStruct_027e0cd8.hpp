86a2d4adcf28004ed058e7cd93f687549eda9d15
Alessevan <44125445+Alessevan@users.noreply.github.com>
2026-06-13 09:51:43 -0400

    fix: undefined 02081d7c

--- include/Unknown/UnkStruct_027e0cd8.hpp
+++ include/Unknown/UnkStruct_027e0cd8.hpp
@@ -5,7 +5,6 @@
 #include "types.h"
 
 #include "MapObject/MapObjectManager.hpp"
-#include "Unknown/UnkStruct_027e09a4.hpp"
 
 class MapObject_20;
 
@@ -145,7 +144,7 @@ public:
     /* 4C */
 
     bool func_ov000_02081e30(unk32 param1, unk32 param2);
-    void func_ov000_02081d7c(unk32 param1, unk32 param2, unk32 param3);
+    void func_ov000_02081d7c(unk32 flagValue, unk32 param2, bool doSet);
     void func_ov000_02081ecc(unk16 param1, unk32 param2);
     void func_ov000_02081eec(unk16 param1, unk32 param2, unk32 param3);
     bool func_ov000_02081f3c(unk16 param1, unk32 param2);
