08d2adcbcb05ab4aa24b97a2ecdf676b91c46079
Alessevan <44125445+Alessevan@users.noreply.github.com>
2026-07-03 00:37:23 +0200

    feat: introduce virtual functions

--- include/MapObject/MapObjectPot.hpp
+++ include/MapObject/MapObjectPot.hpp
@@ -5,26 +5,48 @@
 #include "global.h"
 #include "types.h"
 
-class MapObjectPot : public MapObject {
+class MapObjectPot_Base : public MapObject {
+public:
+    /* 00 (base) */
+    /* 40 */
+
+    MapObjectPot_Base();
+
+    // data_ov000_020b35d0
+
+    /* 00 */ virtual bool vfunc_00();           // func_ov000_0209d780
+    /* 24 */ virtual void vfunc_24() override;  // func_ov000_0209d8e8
+    /* 28 */ virtual unk32 vfunc_28() override; // func_ov000_0209d9f8
+    /* 30 */ virtual ~MapObjectPot_Base() override;
+    /* 38 */ virtual void vfunc_38() override; // func_ov000_0209d9ac
+    /* 3C */ virtual void vfunc_3C() override; // func_ov000_0209db74
+    /* 40 */ virtual void vfunc_40() override; // func_ov000_0209db7c
+    /* 44 */ virtual void vfunc_44() override; // func_ov000_0209d7a4
+    /* 48 */ virtual void vfunc_48() override; // func_ov000_0209da60
+    /* 4C */ virtual void vfunc_4C() override; // func_ov000_0209da70
+    /* 50 */ virtual void vfunc_50() override; // func_ov000_0209dac0
+    /* 54 */ virtual void vfunc_54() override; // func_ov000_0209da64
+};
+
+class MapObjectPot : public MapObjectPot_Base {
 public:
     /* 00 (base) */
     /* 40 */
 
     MapObjectPot();
 
+    /* 04 */ virtual void vfunc_04() override;                                      // func_ov031_02105088
+    /* 14 */ virtual void vfunc_14() override;                                      // func_ov031_021052cc
+    /* 1C */ virtual bool vfunc_1C(ActorRef param1, unk32 param2, VecFx32 *param3); // func_ov031_021050b4
+    /* 24 */ virtual void vfunc_24() override;                                      // func_ov031_02105348
     /* 30 */ virtual ~MapObjectPot() override;
-
-    void func_ov031_02105088();
-    void func_ov031_021050b4();
-    void func_ov031_021051d4();
-    void func_ov031_021052cc();
-    void func_ov031_02105310();
-    void func_ov031_02105348();
-    void func_ov031_021053b4();
-    void func_ov031_021053fc();
+    /* 3C */ virtual void vfunc_3C() override; // func_ov031_021053b4
+    /* 40 */ virtual void vfunc_40() override; // func_ov031_021053fc
+    /* 48 */ virtual void vfunc_48() override; // func_ov031_021051d4
+    /* 54 */ virtual void vfunc_54() override; // func_ov031_02105310
 };
 
-class MapObjectProfilePot : public MapObjectProfile {
+class MapObjectProfilePot : public MapObjectProfile_Derived2 {
 public:
     /* 00 (base) */
 
--- src/031_Land/MapObject/MapObjectPot.cpp
+++ src/031_Land/MapObject/MapObjectPot.cpp
@@ -1,33 +1,35 @@
 #include "MapObject/MapObjectPot.hpp"
 #include "System/SysNew.hpp"
 
+unk32 data_ov031_02118010[0x8];
+
 DECL_PROFILE(MapObjectProfilePot);
 
 MapObject *MapObjectProfilePot::Create() {
-    return new(HeapIndex_2) MapObjectPot();
+    return new(HeapIndex_ITCM) MapObjectPot();
 }
 
 MapObjectProfilePot::MapObjectProfilePot() :
-    MapObjectProfile(MapObjectId_TSUB) {}
+    MapObjectProfile_Derived2(MapObjectId_TSUB, MapObjectId_TSUB) {}
 
 MapObjectPot::MapObjectPot() {}
 
 // non-matching
-void MapObjectPot::func_ov031_02105088() {}
+void MapObjectPot::vfunc_04() {}
 // non-matching
-void MapObjectPot::func_ov031_021050b4() {}
+bool MapObjectPot::vfunc_1C(ActorRef param1, unk32 param2, VecFx32 *param3) {}
 // non-matching
-void MapObjectPot::func_ov031_021051d4() {}
+void MapObjectPot::vfunc_48() {}
 // non-matching
-void MapObjectPot::func_ov031_021052cc() {}
+void MapObjectPot::vfunc_14() {}
 // non-matching
-void MapObjectPot::func_ov031_02105310() {}
+void MapObjectPot::vfunc_54() {}
 // non-matching
-void MapObjectPot::func_ov031_02105348() {}
+void MapObjectPot::vfunc_24() {}
 // non-matching
-void MapObjectPot::func_ov031_021053b4() {}
+void MapObjectPot::vfunc_3C() {}
 // non-matching
-void MapObjectPot::func_ov031_021053fc() {}
+void MapObjectPot::vfunc_40() {}
 
 MapObjectPot::~MapObjectPot() {}
 MapObjectProfilePot::~MapObjectProfilePot() {}
