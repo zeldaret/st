f941168ac703817775186bcb6e1cfea43a8f2903
Mityno <pititcowboy@gmail.com>
2026-08-10 14:24:05 +0200

    feat: Match MapObjectUnkSTAT ctor and decompile its profile, rename vfuncs

--- include/MapObject/MapObjectUnkSTAT.hpp
+++ include/MapObject/MapObjectUnkSTAT.hpp
@@ -4,16 +4,34 @@
 
 #include "MapObject/MapObject.hpp"
 #include "MapObject/MapObjectProfile.hpp"
+#include "Render/ModelRender.hpp"
 #include "global.h"
+#include "nns/g3d/g3d.h"
 #include "types.h"
 
 class MapObjectUnkSTAT : public MapObject {
 public:
     /* 00 (base) */
-    /* 40 */
+    /* 40 */ UnkSystem6_Derived2 mUnk_40;
+    /* 48 */ unk8 mUnk_48;
+    /* 49 */ STRUCT_PAD(0x49, 0x4C);
+    /* 4C */ unk32 mUnk_4C;
+    /* 50 */ unk32 mUnk_50;
+    /* 54 */ unk32 mUnk_54;
+    /* 58 */ unk16 mUnk_58;
+    /* 5A */ unk16 mUnk_5A;
+    /* 5C */ unk16 mUnk_5C;
+    /* 5E */ STRUCT_PAD(0x5E, 0x60);
+    /* 60 */ unk32 mUnk_60;
+    /* 64 */ unk32 mUnk_64;
+    /* 68 */ unk32 mUnk_68;
 
     MapObjectUnkSTAT();
 
+    /* 00 */ virtual bool vfunc_00() override; // Init
+    /* 08 */ virtual void vfunc_08() override;
+    /* 0C */ virtual void vfunc_0C() override;
+    /* 14 */ virtual void vfunc_14() override;
     /* 30 */ virtual ~MapObjectUnkSTAT() override;
 
     void func_ov063_0215f6d0(void);
@@ -25,9 +43,25 @@ public:
     void func_ov063_0215fce0(void);
 };
 
-class MapObjectProfileUnkSTAT : public MapObjectProfile {
+class MapObjectProfileUnkSTAT_Base_D4 : public MapObject_10 {
+public:
+};
+
+class MapObjectProfileUnkSTAT_Base : public MapObjectProfile_Derived2 {
 public:
     /* 00 (base) */
+    /* D4 */ MapObjectProfileUnkSTAT_Base_D4 mUnk_D4;
+    /* F8 */
+
+    MapObjectProfileUnkSTAT_Base(MapObjectId mapObjId1, MapObjectId mapObjId2) :
+        MapObjectProfile_Derived2(mapObjId1, mapObjId2),
+        mUnk_D4() {};
+};
+
+class MapObjectProfileUnkSTAT : public MapObjectProfileUnkSTAT_Base {
+public:
+    /* 00 (base) */
+    /* F8 */
 
     MapObjectProfileUnkSTAT();
     ~MapObjectProfileUnkSTAT();
--- src/063_Shrine/MapObject/MapObjectUnkSTAT.cpp
+++ src/063_Shrine/MapObject/MapObjectUnkSTAT.cpp
@@ -1,7 +1,12 @@
 //! TODO: This file was generated automatically and might contain errors
 
 #include "MapObject/MapObjectUnkSTAT.hpp"
+#include "MapObject/MapObjectProfile.hpp"
 #include "System/SysNew.hpp"
+#include "flags.h"
+#include "nitro/math.h"
+
+extern MapObject_10 data_ov063_021644e4;
 
 DECL_PROFILE(MapObjectProfileUnkSTAT);
 
@@ -10,16 +15,38 @@ MapObject *MapObjectProfileUnkSTAT::Create() {
 }
 
 MapObjectProfileUnkSTAT::MapObjectProfileUnkSTAT() :
-    MapObjectProfile(MapObjectId_STAT) {}
+    MapObjectProfileUnkSTAT_Base(MapObjectId_STAT, MapObjectId_STAT) {
+    mUnk_D4.mUnk_08 = 0x7007;
+    VecFx32_Init(0, 0, 0, &mUnk_D4.mUnk_0C);
+    mUnk_D4.mUnk_18.x = 0x800;
+    mUnk_D4.mUnk_18.y = 0x1000;
 
-MapObjectUnkSTAT::MapObjectUnkSTAT() {}
+    mUnk_06 = -1;
+    UNSET_FLAG2(mUnk_1E, 0);
+}
 
-void MapObjectUnkSTAT::func_ov063_0215f6d0(void) {}
-void MapObjectUnkSTAT::func_ov063_0215f710(void) {}
-void MapObjectUnkSTAT::func_ov063_0215f798(void) {}
+MapObjectUnkSTAT::MapObjectUnkSTAT() :
+    mUnk_40(G3d_GetModelPtr(GET_PROFILE(MapObjectProfileUnkSTAT)->mUnk_20.mUnk_50)),
+    mUnk_48(1),
+    mUnk_4C(4),
+    mUnk_50(-1),
+    mUnk_54(0x8000),
+    mUnk_58(500),
+    mUnk_5A(0),
+    mUnk_5C(0),
+    mUnk_60(0),
+    mUnk_64(0),
+    mUnk_68(0) {
+    mUnk_10 = &data_ov063_021644e4;
+}
+
+bool MapObjectUnkSTAT::vfunc_00(void) {}
+void MapObjectUnkSTAT::vfunc_08(void) {}
+void MapObjectUnkSTAT::vfunc_0C(void) {}
+void MapObjectUnkSTAT::vfunc_14(void) {}
+
 void MapObjectUnkSTAT::func_ov063_0215f7ac(void) {}
 void MapObjectUnkSTAT::func_ov063_0215fc40(void) {}
-void MapObjectUnkSTAT::func_ov063_0215fc88(void) {}
 void MapObjectUnkSTAT::func_ov063_0215fce0(void) {}
 
 MapObjectUnkSTAT::~MapObjectUnkSTAT() {}
