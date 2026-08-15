599db72c1b8eb946ed85ca7035a02078f26195fe
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2026-08-15 03:03:37 +0200

    misc: add the new versions to configure.py and add new macros in versions.h

--- include/versions.h
+++ include/versions.h
@@ -3,6 +3,7 @@
 #define EUR 0
 #define JP 1
 #define USA 2
+#define EUR1 3
 
 #ifndef VERSION
     #warning "VERSION is not defined, check configure.py!"
@@ -10,5 +11,6 @@
 #endif
 
 #define IS_EUR (VERSION == EUR)
+#define IS_EUR1 (VERSION == EUR1)
 #define IS_JP (VERSION == JP)
 #define IS_USA (VERSION == USA)
